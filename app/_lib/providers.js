b5566c3b0636fd9a935903352511ed6503d28848
Mostafa <mostafaqaraman@outlook.com>
2025-04-10 00:13:20 +0200

    Modified Google Provider to always ask user which account to log in

--- app/_lib/providers.js
+++ app/_lib/providers.js
@@ -4,5 +4,12 @@ export const providers = [
   GoogleProvider({
     clientId: process.env.GOOGLE_CLIENT_ID,
     clientSecret: process.env.GOOGLE_CLIENT_SECRET,
+    authorization: {
+      params: {
+        prompt: "select_account",
+        access_type: "offline",
+        response_type: "code",
+      },
+    },
   }),
 ];
