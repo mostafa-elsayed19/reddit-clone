f62a86c461aa5b510e95d39fb4bc6b66deb33be0
Mostafa <mostafaqaraman@outlook.com>
2025-04-13 16:33:22 +0200

    Update: small modification in button design

--- app/_components/Button.jsx
+++ app/_components/Button.jsx
@@ -11,7 +11,7 @@ function Button({
       type={type}
       onClick={onClick}
       disabled={disabled}
-      className={`rounded-md bg-blue-500 px-4 py-2 text-white hover:bg-blue-600 focus:ring-2 focus:ring-blue-500 focus:outline-none ${
+      className={`rounded-md bg-blue-500 px-4 py-2 text-white hover:bg-blue-600 focus:ring-2 focus:ring-blue-700 focus:outline-none ${
         disabled ? "cursor-not-allowed opacity-50" : "cursor-pointer"
       } ${fullWidth ? "w-full" : ""} ${className}`}
     >
