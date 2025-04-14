d13dd2cdc3e8adcc2d7cdd998f8e0d53bf9acf67
Mostafa <mostafaqaraman@outlook.com>
2025-04-14 16:40:54 +0200

    Modified Home page

--- app/page.jsx
+++ app/page.jsx
@@ -1,7 +1,7 @@
-import Wrapper from "./_components/Wrapper";
+import Wrapper from "@/_components/Wrapper";
 import Navbar from "@/_components/Navbar";
 import AddPost from "@/_components/AddPost";
-import PostsList from "./_components/PostsList";
+import PostsList from "@/_components/PostsList";
 
 export default async function Home() {
   return (
